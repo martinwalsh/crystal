cfade272cb3bab1ac017162dd5d618cccf67d764
Martin Walsh <marty@notoptimal.com>
2018-05-17 18:40:30 -0500

    run-length-encoding: Use JSON.mapping for Generators

--- generator/src/generators/run_length_encoding.cr
+++ generator/src/generators/run_length_encoding.cr
@@ -1,52 +1,23 @@
-require "../exercise_generator"
-require "../exercise_test_case"
+require "../generator"
 
-class RunLengthEncodingGenerator < ExerciseGenerator
-  def exercise_name
-    "run-length-encoding"
+class RunLengthEncodingTestCase
+  class Input
+    JSON.mapping({string: String})
   end
 
-  def test_cases
-    JSON.parse(data)["cases"].flat_map do |case_group|
-      case_group["cases"].map do |test_case|
-        if test_case["property"] == "consistency"
-          RunLengthEncodingConsistencyTestCase.new(test_case)
-        else
-          RunLengthEncodingTestCase.new(test_case)
-        end
-      end
+  include Exercise::TestCase(Input, String)
+
+  def test_workload
+    if test_method == "consistency"
+      "#{test_class}.decode(#{test_class}.encode(\"#{input.string}\")).should eq(\"#{expected}\")"
+    else
+      "#{test_class}.#{test_method}(\"#{input.string}\").should eq(\"#{expected}\")"
     end
   end
-end
 
-class RunLengthEncodingTestCase < ExerciseTestCase
-  private getter description : JSON::Any
-  private getter input : JSON::Any
-  private getter expected : JSON::Any
-  private getter method : JSON::Any
-
-  def initialize(test_case)
-    @description = test_case["description"]
-    @input = test_case["input"]["string"]
-    @expected = test_case["expected"]
-    @method = test_case["property"]
-  end
-
-  def workload
-    "RunLengthEncoding.#{method}(\"#{input}\").should eq(\"#{expected}\")"
-  end
-
-  def test_name
-    method.as_s + " " + description.as_s
+  def test_description
+    test_method == "consistency" ? description : "#{test_method} #{description}"
   end
 end
 
-class RunLengthEncodingConsistencyTestCase < RunLengthEncodingTestCase
-  def workload
-    "RunLengthEncoding.decode(RunLengthEncoding.encode(\"#{input}\")).should eq(\"#{expected}\")"
-  end
-
-  def test_name
-    description
-  end
-end
+Generator.register :RunLengthEncoding
