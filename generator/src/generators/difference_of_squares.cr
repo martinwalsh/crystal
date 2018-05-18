0e5c3d2aae363f3607d613e51303394597e05944
Martin Walsh <marty@notoptimal.com>
2018-05-17 19:22:01 -0500

    difference-of-squares: Use JSON.mapping for Generators

--- generator/src/generators/difference_of_squares.cr
+++ generator/src/generators/difference_of_squares.cr
@@ -1,38 +1,16 @@
-require "../exercise_generator"
-require "../exercise_test_case"
+require "../generator"
 
-class DifferenceOfSquaresGenerator < ExerciseGenerator
-  def exercise_name
-    "difference-of-squares"
+class DifferenceOfSquaresTestCase
+  class Input
+    JSON.mapping(number: Int32)
   end
 
-  def test_cases
-    JSON.parse(data)["cases"].flat_map do |case_group|
-      case_group["cases"].map do |test_case|
-        DifferenceOfSquaresTestCase.new(test_case)
-      end
-    end
+  include TestDSL
+  include Exercise::TestCase(Input, Int32)
+
+  _it "calculates #{description} is #{expected}" do
+    "Squares.#{test_method}(#{input.number}).should eq(#{expected})"
   end
 end
 
-class DifferenceOfSquaresTestCase < ExerciseTestCase
-  private getter number : JSON::Any
-  private getter description : JSON::Any
-  private getter expected : JSON::Any?
-  private getter type : String
-
-  def initialize(test_case)
-    @type = test_case["property"].as_s.gsub(/([A-Z])/, "_\\1").downcase
-    @number = test_case["input"]["number"]
-    @description = test_case["description"]
-    @expected = test_case["expected"]?
-  end
-
-  def workload
-    "Squares.#{type}(#{number}).should eq(#{expected})"
-  end
-
-  def test_name
-    "calculates #{description} is #{expected}"
-  end
-end
+Generator.register :DifferenceOfSquares
